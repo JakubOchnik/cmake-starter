7fe64a3f4821243f10bcf2825d09340c32e37ecb
JakubOchnik <jakub.ochnik1@gmail.com>
2022-01-27 22:14:10 +0100

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+
+/**
+ * @brief Main function
+ * 
+ * @param argc number of input arguments
+ * @param argv array of input arguments
+ * @return int return code
+ */
+
+int main(int argc, char* argv[])
+{
+    return 0;
+}
